19af3ac33b7e1b2b6c231ba924447d07512aff59
blaj95 <32546217+blaj95@users.noreply.github.com>
2017-12-01 18:03:40 -0500

    vr
    vr

--- Assets/Scripts/DisableScript.cs
+++ Assets/Scripts/DisableScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DisableScript : MonoBehaviour {
 
-    public GameObject[] objects;
+    public GameObject vrPlayer;
 	// Use this for initialization
 	void Start ()
     {
@@ -16,9 +16,6 @@ public class DisableScript : MonoBehaviour {
     {
         PhotonView pv = PhotonView.Get(this);
         if (PhotonNetwork.connected == true && pv.isMine == true)
-		foreach(GameObject go in objects)
-        {
-            go.SetActive(false);
-        }
+            vrPlayer.GetComponent<Camera>().enabled = false;
 	}
 }
