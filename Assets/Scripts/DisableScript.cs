7a8730ea25834499c790e0da5912da78b2144ef1
blaj95 <32546217+blaj95@users.noreply.github.com>
2017-12-01 17:46:26 -0500

    vr
    vr

---
+++ Assets/Scripts/DisableScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableScript : MonoBehaviour {
+
+    public GameObject[] objects;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PhotonView pv = PhotonView.Get(this);
+        if (PhotonNetwork.connected == true && pv.isMine == true)
+		foreach(GameObject go in objects)
+        {
+            go.SetActive(false);
+        }
+	}
+}
