233858e15e426cad8bfcaab556117b39ff47c833
blaj95 <32546217+blaj95@users.noreply.github.com>
2017-12-01 17:56:57 -0500

    vr
    vr

---
+++ Assets/Scripts/EnableScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableScript : MonoBehaviour {
+
+    public GameObject[] objects;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PhotonView pv = PhotonView.Get(this);
+		foreach(GameObject go in objects)
+        {
+            if (PhotonNetwork.connected == true && pv.isMine == true)
+            {
+                go.SetActive(true);
+            }
+        }
+	}
+}
