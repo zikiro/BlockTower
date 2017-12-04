25422d053d98bf10d3c6e3264925666073e2800b
blaj95 <32546217+blaj95@users.noreply.github.com>
2017-12-04 18:23:57 -0500

    vr
    vr

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -6,19 +6,23 @@ public class TouchController : Photon.MonoBehaviour {
 
     //private Vector3 networkPosition;
     //private Quaternion networkRotation;
+    public GameObject RightHand;
     private Vector3 realPosition;
     private Quaternion realRotation;
+    public Transform RcontrollerLocal;
+    public Transform RcontrollerGlobal;
 
     // Use this for initialization
     void Start()
     {
-
+        RcontrollerGlobal = GameObject.Find("RHand").transform;
+        RcontrollerLocal = RcontrollerGlobal.Find("Touch_controller_R_luxury");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
         transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
         
@@ -31,18 +35,22 @@ public class TouchController : Photon.MonoBehaviour {
 
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (stream.isWriting == true)
+        if (stream.isWriting)
         {
 
 
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
+            stream.SendNext(RcontrollerGlobal.position);
+            stream.SendNext(RcontrollerGlobal.rotation);
+            stream.SendNext(RcontrollerLocal.localPosition);
+            stream.SendNext(RcontrollerLocal.localRotation);
 
         }
         else 
         {
-            transform.position = (Vector3)stream.ReceiveNext();
-            transform.rotation = (Quaternion)stream.ReceiveNext();
+           this.transform.position = (Vector3)stream.ReceiveNext();
+           this.transform.rotation = (Quaternion)stream.ReceiveNext();
+            RightHand.transform.position = (Vector3)stream.ReceiveNext();
+            RightHand.transform.rotation = (Quaternion)stream.ReceiveNext();
         }
     }
 
--- Assets/Scripts/TouchControllerLeft.cs
+++ Assets/Scripts/TouchControllerLeft.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TouchControllerLeft : Photon.MonoBehaviour {
+public class TouchControllerLeft : Photon.MonoBehaviour
+{
 
 
 
     //private Vector3 networkPosition;
     //private Quaternion networkRotation;
+    public GameObject LeftHand;
     private Vector3 realPosition;
     private Quaternion realRotation;
+    public Transform LcontrollerLocal;
+    public Transform LcontrollerGlobal;
 
     // Use this for initialization
     void Start()
     {
-
+        LcontrollerGlobal = GameObject.Find("LHand").transform;
+        LcontrollerLocal = LcontrollerGlobal.Find("Touch_controller_L_luxury");
     }
 
     // Update is called once per frame
@@ -23,9 +28,8 @@ public class TouchControllerLeft : Photon.MonoBehaviour {
 
         transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
         transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
-        realRotation = transform.rotation;
-        realPosition = transform.position;
-           
+
+
 
         //transform.position = Vector3.Lerp(transform.localPosition, realPosition, Time.deltaTime * 10);
         //transform.rotation = Quaternion.Lerp(transform.localRotation, realRotation, Time.deltaTime * 10);
@@ -38,16 +42,18 @@ public class TouchControllerLeft : Photon.MonoBehaviour {
         {
 
 
-            stream.SendNext(realPosition);
-            stream.SendNext(realRotation);
+            stream.SendNext(LcontrollerGlobal.position);
+            stream.SendNext(LcontrollerGlobal.rotation);
+            stream.SendNext(LcontrollerLocal.localPosition);
+            stream.SendNext(LcontrollerLocal.localRotation);
 
         }
-        else if (stream.isReading)
+        else
         {
-            realPosition = (Vector3)stream.ReceiveNext();
-            realRotation = (Quaternion)stream.ReceiveNext();
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            this.transform.rotation = (Quaternion)stream.ReceiveNext();
+            LeftHand.transform.position = (Vector3)stream.ReceiveNext();
+            LeftHand.transform.rotation = (Quaternion)stream.ReceiveNext();
         }
     }
-
-
 }
