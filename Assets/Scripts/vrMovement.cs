30f9d4e9330efeab2b9ead9d215630db390ca9a8
blaj95 <32546217+blaj95@users.noreply.github.com>
2017-12-05 13:37:05 -0500

    vr
    vr

---
+++ Assets/Scripts/vrMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vrMovement : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Button.Two))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime*speed);
+            Debug.Log("AAA");
+        }
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+            Debug.Log("AAA");
+        }
+    }
+}
