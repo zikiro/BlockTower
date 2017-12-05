683f327938d848bdc8f1b337d588978b0ff3a628
blaj95 <32546217+blaj95@users.noreply.github.com>
2017-12-05 14:55:18 -0500

    vrPrefab movement done
    vr player can float around tower

---
+++ Assets/vrPlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vrPlayerMovement : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Button.Two))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+	}
+}
